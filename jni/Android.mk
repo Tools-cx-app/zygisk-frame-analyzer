da35775c73d094c58318f96fc5259a3658d522a5
Tools-app <localhost.hutao@gmail.com>
2025-11-08 09:16:22 +0800

    fix: fix unsupport *.cpp
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,7 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := zygisk-frame-analyzer
-LOCAL_SRC_FILES := ../src/*.cpp
+LOCAL_SRC_FILES := \
+    ../src/lib.cpp
 LOCAL_CFLAGS := -std=c++2b -O3
 LOCAL_LDFLAGS := -static-libstdc++
 LOCAL_LDLIBS := -llog
